0c4da51a5df99e99a5265c8eb4e09caa30b17c57
shakibzaman <zaman.shakib@gmail.com>
2022-11-07 12:23:12 +0600

    Correction

---
+++ app/OrderReturn.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderReturn extends Model
+{
+    use HasFactory;
+    protected $table = 'order_returns';
+}
